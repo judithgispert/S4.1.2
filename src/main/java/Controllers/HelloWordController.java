b6b6f8ffc1ee3703cb6d2f96f46d51cd0cb2de77
judithgispert <jugispert@gmail.com>
2024-01-12 14:24:40 +0100

    Controller class with hello methods

---
+++ src/main/java/Controllers/HelloWordController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+    @GetMapping("/HelloWord")
+    public String greet(@RequestParam(defaultValue = "UNKNOWN") String name){
+        return "Hello, " + name + ". You're running a Maven project.";
+    }
+
+    @GetMapping({"/HelloWord2", "/HelloWord2/{name}"})
+    public String greet2(@PathVariable(required = false) String name){
+        if (name != null){
+            return "Hello, " + name + ". You're running a Maven project.";
+        } else {
+            return "Name missing.";
+        }
+    }
+}
