beb7b06f5f9d32be5277e6725c956ee590d4addf
judithgispert <jugispert@gmail.com>
2024-01-15 10:13:20 +0100

    Change package

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/Controllers/HelloWordController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/Controllers/HelloWordController.java
@@ -1,4 +1,4 @@
-package Controllers;
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
